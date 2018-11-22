138bd7e56fb996e4e159da2c6af7bc4d0d9536c3
Max <maximko98@gmail.com>
2018-11-23 01:05:28 +0300

    Возможность запуска приложения в консольном режим и gui

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from movie_cutter import MovieCutterAPI
+from interface import GUI
 import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Automatically cuts silent periods from video file',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--input', type=argparse.FileType('r'), help='path to input file', required=True)
+    parser.add_argument('-g', '--gui', action='store_true',
+                        help='run in GUI mode, if selected, all other flags will be ignored')
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'), help='path to input file')
     parser.add_argument('-o', '--output', type=argparse.FileType('w'), help='path where output file will be saved',
                         default='./cut.mp4')
     parser.add_argument('-m', '--min', type=int, help='minimal duration of silent part to be cut (in milliseconds)',
@@ -14,8 +17,13 @@ if __name__ == '__main__':
                         default=-16)
 
     args = parser.parse_args()
-
-    cutter = MovieCutterAPI(source_file=args.input.name, output_file=args.output.name, min_silence_len=args.min,
-                            silence_thresh=args.thresh)
-    cutter.cut()
-    cutter.save_clip()
+    if (args.gui):
+        GUI()
+    else:
+        if (args.input is None):
+            parser.error("the following arguments are required: -i/--input")
+        else:
+            cutter = MovieCutterAPI(source_file=args.input.name, output_file=args.output.name, min_silence_len=args.min,
+                                    silence_thresh=args.thresh)
+            cutter.cut()
+            cutter.save_clip()
